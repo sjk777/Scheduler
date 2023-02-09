348cf23a80fba984e0241bc0daf87e384344d89a
Sky K <skyj.kim7@gmail.com>
2023-02-09 09:03:40 -0800

    added variables for every hour in the block

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,31 @@
 var thisTime = dayjs();
-var thisHour = dayjs().hour()
-var saveButton = $(".saveBtn")
+var thisHour = dayjs().hour();
+var saveButton = $(".saveBtn");
+
+var hour1 = $("hour-1");
+var hour2 = $("hour-2");
+var hour3 = $("hour-3");
+var hour4 = $("hour-4");
+var hour5 = $("hour-5");
+var hour6 = $("hour-6");
+var hour7 = $("hour-7");
+var hour8 = $("hour-8");
+var hour9 = $("hour-9");
+var hour10 = $("hour-10");
+var hour11 = $("hour-11");
+var hour12 = $("hour-12");
+var hour13 = $("hour-13");
+var hour14 = $("hour-14");
+var hour15 = $("hour-15");
+var hour16 = $("hour-16");
+var hour17 = $("hour-17");
+var hour18 = $("hour-18");
+var hour19 = $("hour-19");
+var hour20 = $("hour-20");
+var hour21 = $("hour-21");
+var hour22 = $("hour-22");
+var hour23 = $("hour-23");
+var hour24 = $("hour-24");
 
 
 
