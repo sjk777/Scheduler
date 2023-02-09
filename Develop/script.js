8e07330a87e972e9800adde60b160118a2c7e5e8
Sky K <skyj.kim7@gmail.com>
2023-02-09 09:01:10 -0800

    added variables for time hour and the save button to the script page

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
-var today = dayjs();
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var thisTime = dayjs();
+var thisHour = dayjs().hour()
+var saveButton = $(".saveBtn")
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,6 +23,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  $("#calendarDate").text(today.format("MMM D, YYYY"));
+  $("#calendarDate").text(thisTime.format("MMM D, YYYY"));
 
 });
